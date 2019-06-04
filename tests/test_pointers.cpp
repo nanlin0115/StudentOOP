3b2f6df3da037db2e07d396fe155ce962ee8c0f3
Nan <nanlin@10-18-240-32.dynapool.wireless.nyu.edu>
2019-06-04 11:30:00 -0400

    testing new code

---
+++ tests/test_pointers.cpp
@@ -0,0 +1,45 @@
+//
+//  test_pointers.cpp
+//  hwandlab
+//
+//  Created by Nan Lin on 6/4/19.
+//  Copyright © 2019 Nan Lin. All rights reserved.
+//
+
+//#include "test_pointers.hpp"
+#include <cassert>
+#include <iostream>
+#include <cassert>
+#include "../my_code/pointers.h"
+
+using namespace std;
+
+const int NUM_THINGS = 40;
+
+
+int main() {
+    // first we test the various methods of passing args:
+    int n = 0;
+    inc_by_value(n);
+    assert(n == 0);
+    inc_with_pointer(&n);
+    assert(n == 1);
+    inc_with_reference(n);
+    assert(n == 2);
+    
+    // now let's make, print, change, and delete some "things":
+    Thing** things = create_array_of_things(NUM_THINGS);
+    assert(things != nullptr);
+    
+    print_all_things(things, NUM_THINGS);
+    
+    double_all_things(things, NUM_THINGS);
+    for (int i = 0; i < NUM_THINGS; i++)
+        assert(things[i]->val == (i * 2));
+    print_all_things(things, NUM_THINGS);
+    delete_all_things(things, NUM_THINGS);
+    
+    // now assign TAs to students
+    
+    return 0;
+}
